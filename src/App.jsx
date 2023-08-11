9d31a1fcab3f543575a254c1ca07b738cde9609a
Yonathan <yonathan@Yonathans-Air.localdomain>
2023-08-11 18:12:48 +0700

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import "./castom.css";
 
 import Header from "./componets/Header";
-// import Footer from "./componets/Footer";
+import Footer from "./componets/Footer";
 import Home from "./pages/main/Home";
 import About from "./pages/main/About";
 import Projects from "./pages/main/Projects";
